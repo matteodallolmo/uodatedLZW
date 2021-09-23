1d31fe21abed8f22ad4d1ef057678821dfb8a497
Matteo Dall'Olmo <matteo.dallolmo1@gmail.com>
2021-09-22 21:05:55 -0700

    removed uneccessary libraries to save space

--- LZW.java
+++ LZW.java
@@ -1,7 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
